5b713a23f8cee6540d250712f943ad64951d5982
Chip <nicolas.chipana@utec.edu.pe>
2018-10-30 22:45:24 -0500

    Terminado

--- binary.cpp
+++ binary.cpp
@@ -1,33 +1,60 @@
 #ifndef BOOLEAN_H
 #define BOOLEAN_H
 
+#include <iostream>
+using namespace std;
+
 class boolean {
     private:
         char* array;
         int size;
-    
+
     public:
-        boolean(int size) {
-            // TODO
+        boolean(int size1):array(nullptr),size((size1/8)+1){
+          array = new char[size];
+          for(int i = 0; i < size; i++){
+            array[i] = 0x00;
+          }
+          // TODO
         }
-        
+
         void bitOn(int position) {
+          char mask = 0x01;
+          mask = mask << position % 8;
+          array[position/8] = array[position/8] | mask;
             // TODO
         }
-             
+
         void bitOff(int position) {
+          char mask = 0x01;
+          mask = mask << position % 8;
+          mask = ~mask;
+          array[position/8] = array[position/8] & mask;
             // TODO
         }
-             
+
         bool operator[](int position) {
+          char mask = 0x01;
+          mask = mask << position%8;
+          array[position/8] = array[position/8] & mask;
+          if(array[position/8] == 0){
+            return false;
+          }
+          else{
+            return true;
+          }
             // TODO
         }
-    
-        void clear() {    
+
+        void clear() {
+          for(int i = 0; i < size; i++){
+            array[i] = 0x00;
+          }
             // TODO
         }
-             
+
         ~boolean() {
+          delete array;
             // TODO
         }
 };
--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
+#include "binary.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
-    cout << "\Binary Array Practice" << endl;
-    cout << "===========================================================" << endl << endl;
+    cout << "Binary Array Practice" << endl;
+    cout << "===========================================================" << endl;
 
-    system("read");
+    boolean MyBool(64);
+    MyBool.bitOn(3);
+    MyBool.bitOn(4);
+    MyBool.bitOn(53);
+    MyBool.bitOff(3);
+    MyBool.bitOff(4);
+    if(MyBool[53])
+      cout << "Es verdadero"<<endl;
+    else
+      cout << "Es falso"<<endl;
+
     return EXIT_SUCCESS;
 }
-
